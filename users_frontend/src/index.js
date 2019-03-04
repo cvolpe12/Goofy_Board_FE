4d2be61880f9be794aa9b5a5bd2e09d2ac992c05
cvolpe12 <christopher.volpe12@gmail.com>
2019-03-04 17:36:24 -0500

    added container

--- users_frontend/src/index.js
+++ users_frontend/src/index.js
@@ -2,48 +2,52 @@ document.addEventListener("DOMContentLoaded", () => {
   const snowboarder = document.querySelector('#character')
 
   document.addEventListener("keydown", moveCharacter)
+  document.addEventListener("click", (e)=> {console.log(e);})
 
-
-function moveCharacter(e) {
-  console.log(e);
-  switch (e.code) {
-    case "ArrowRight":
-      moveRight()
-      break;
-    case "ArrowLeft":
-      moveLeft()
-      break;
-    case "Space":
-    debugger
-      jumpUp()
-      break;
-    default:
+  function moveCharacter(e) {
+    // console.log(e);
+    switch (e.code) {
+      case "ArrowRight":
+        e.preventDefault()
+        moveRight()
+        // console.log(snowboarder);
+        break;
+      case "ArrowLeft":
+        e.preventDefault()
+        moveLeft()
+        console.log(snowboarder);
+        break;
+      case "Space":
+        e.preventDefault()
+        jumpUp()
+        console.log(snowboarder);
+        break;
+      default:
+    }
   }
-}
-
   function moveRight(){
-    snowboarder.style.left = parseInt(snowboarder.style.left) + 5 + 'px';
+    console.log(snowboarder.style.left);
+    if (parseInt(snowboarder.style.left) >= 700) {
+      snowboarder.style.left = 700 + 'px'
+    } else {
+      snowboarder.style.left = parseInt(snowboarder.style.left) + 5 + 'px';
+    }
   }
-
   function moveLeft(){
-    snowboarder.style.left = parseInt(snowboarder.style.left) - 5 + 'px';
+    if (parseInt(snowboarder.style.left) <= 0) {
+      snowboarder.style.left = 0 + 'px'
+    } else {
+      snowboarder.style.left = parseInt(snowboarder.style.left) - 5 + 'px';
+    }
   }
-
   function jumpUp(){
-    snowboarder.style.top = parseInt(snowboarder.style.top) - 10 + 'px';
-    // wait(1000)
-    snowboarder.style.top = parseInt(snowboarder.style.top) + 10 + 'px';
+    var x = 0;
+
+    var interval = setInterval(function() {
+        x++;
+        snowboarder.style.top = 250 - (-0.1 * x * (x - 50)) + 'px';
+
+        if(x >= 50) clearInterval(interval);
+    }, 20);
   }
-
-
-
-  function wait(ms){
-   var start = new Date().getTime();
-   var end = start;
-   while(end < start + ms) {
-     end = new Date().getTime();
-  }
-}
-
-
 })
