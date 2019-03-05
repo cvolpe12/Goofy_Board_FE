eec4fcee5c75fb3e8b33b0f6acda9d37b0166556
Sarah Shin <sarahsujin.shin@gmail.com>
2019-03-05 15:52:24 -0500

    splat disappears

--- users_frontend/src/index.js
+++ users_frontend/src/index.js
@@ -1,18 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   //VARIABLES===================================================================
   const gameCanvas = document.querySelector("#game-canvas")
-  const highScoreList = document.querySelector("#score-list")
+  const gameScreen = document.querySelector('#game-screen')
+  const dead = document.querySelector('#splat')
+  const bug = document.querySelector('#bug')
+  var ctx = gameCanvas.getContext("2d")
 
   let score = 0
-  //CALLS=======================================================================
-  fetchHighScores()
+
+  // gameCanvas.addEventListener("click", (e)=>{
+  //   console.log(e.target);
+  // })
 
   //EVENT LISTENERS=============================================================
-  gameCanvas.addEventListener("click",(e)=>{
-    console.log("click!");
-  })
+  gameScreen.addEventListener("click", splat)
+
 
   //FUNCTIONS===================================================================
+  function spawnEnemy(){
+    let enemy = bug
+    let x = Math.random() * 850
+    let y = Math.random() * 450
+    if ((y > 60 && y < 450) && (x > 10 && x < 850)) {
+      enemy.style = `position:absolute; left:${x}px; top: ${y}px;`
+    } else {
+      spawnEnemy()
+    }
+  }
 
+  function splat(e){
+    console.log(e.target);
+    if (e.target.id === "bug") {
+      e.target.src = "images/red_splat.png"
+      console.log("splat");
+    }
+    setTimeout(function(){
+      debugger
+      var splatImg = gameScreen.querySelector('#bug');
+      gameScreen.removeChild(splatImg)
+    }, 1000)
+  }
 
+  //CALLS=======================================================================
+  spawnEnemy()
 })
