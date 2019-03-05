2c72f24e1aed0953a086b8f896b5e5e72250a4a5
cvolpe12 <christopher.volpe12@gmail.com>
2019-03-05 18:15:59 -0500

    increased difficulty

--- users_frontend/src/index.js
+++ users_frontend/src/index.js
@@ -4,12 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const gameScreen = document.querySelector('#game-screen')
   const dead = document.querySelector('#splat')
   const bug = document.querySelector('#bug')
-  var ctx = gameCanvas.getContext("2d")
+  const scoreCount = document.querySelector('#score')
   let bugId = 1
-  let score = 0
+  let hit = []
+  let miss = []
+  let screenBugs = []
 
-  // Class======================================================================
-
+  document.addEventListener("click", (e)=>{console.log(e)})
 
   //EVENT LISTENERS=============================================================
   gameScreen.addEventListener("click", splat)
@@ -17,38 +18,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //FUNCTIONS===================================================================
   function spawnEnemy(){
+    let time = 0
     let enemy = new Image(100, 100)
-    // let enemy = bug
-    enemy.onload = {
-
-    }
     enemy.src = "images/ahh_bug.png"
     enemy.id = "bug"
     enemy.dataset.id = bugId++
     let x = Math.random() * 850
-    let y = Math.random() * 450
-    if ((y > 60 && y < 450) && (x > 10 && x < 850)) {
+    let y = Math.random() * 590
+    //if bug doesn't collide with div && doesn't collide with another bug
+    if ((y > 100 && y < 590) && (x > 10 && x < 850)) {
       enemy.style = `position:absolute; left:${x}px; top: ${y}px;`
       gameScreen.appendChild(enemy)
+      screenBugs.push(enemy)
     } else {
       spawnEnemy()
     }
+    console.log(screenBugs);
+    //
+    //
+    //
     // debugger
   }
 
-  function multipleEnemies(){
+  function multipleEnemies(time){
     var times = 1;
+    // debugger
     for(var i=0; i < times; i++){
-      setInterval(spawnEnemy, 1000)
-      // debugger
+      setInterval(spawnEnemy, time)
+      console.log(time);
     }
   }
 
   function splat(e){
-    console.log(e.target);
+    // console.log(e.target);
     if (e.target.id === "bug") {
       e.target.src = "images/red_splat.png"
-      console.log("splat");
+      e.target.id = "splat"
+      // console.log("splat");
+      score(e.target)
     }
     setTimeout(function(){
       // debugger
@@ -57,7 +64,22 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 1000)
   }
 
+  function score(deadBug){
+    hit.push(deadBug)
+    if(hit.length%10 === 0) {
+      speedUp()
+    }
+    scoreCount.innerHTML = `Score: ${hit.length}`
+  }
 
+  function speedUp(){
+    if(hit.length < 10){
+      multipleEnemies(2000)
+    } else if (hit.length >= 10){
+      multipleEnemies(1000)
+    }
+  }
   // spawnEnemy()
-  // multipleEnemies()
-})
+  // speedUp()
+
+}) //end of DOMContentLoaded
